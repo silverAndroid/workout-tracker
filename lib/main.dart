57fa063d046cb115872cd140f29094fb44cdae8a
Rushil Perera <pererarushil1940@gmail.com>
2017-03-12 22:44:42 -0400

    Initializes DB if it doesn't exist

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'dart:async';
 
 import 'platform_method.dart';
 import 'workout_view.dart';
@@ -10,10 +11,13 @@ void main() {
 
 class MyApp extends StatelessWidget {
   String _title = 'Workout Tracker';
-  PlatformMethodChannel dbPlatform = new PlatformMethod().dbPlatform;
 
-  void initDB() {
-    dbPlatform.invokeMethod('initDB');
+  Future initDB() async {
+    PlatformMethod platformMethod = new PlatformMethod();
+    await platformMethod.dbPlatform.invokeMethod('initDB');
+    await platformMethod.rawQuery('CREATE TABLE IF NOT EXISTS WORKOUTS(ID INTEGER PRIMARY KEY NOT NULL, NAME TEXT UNIQUE NOT NULL, DESCRIPTION TEXT);', [], true, true);
+    await platformMethod.rawQuery('CREATE TABLE IF NOT EXISTS EXERCISES(ID INTEGER PRIMARY KEY NOT NULL, NAME TEXT UNIQUE NOT NULL, BODY_GROUP TEXT NOT NULL, DESCRIPTION TEXT, RECOMMENDED_REPS INTEGER DEFAULT -1, RECOMMENDED_SETS INTEGER DEFAULT -1);', [], true, true);
+    await platformMethod.rawQuery('CREATE TABLE IF NOT EXISTS SETS(ID INTEGER PRIMARY KEY NOT NULL, NUM_REPS INTEGER NOT NULL, WEIGHT INTEGER NOT NULL, EXERCISE_ID INTEGER NOT NULL, FOREIGN KEY (EXERCISE_ID) REFERENCES EXERCISES (ID));', [], true, true);
   }
 
   // This widget is the root of your application.
